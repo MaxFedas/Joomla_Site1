1daa887c2c5270b0452eba4b35cb4ff21182f63c
DarkOverkill <maxym.fedas@gmail.com>
2016-02-05 10:35:28 +0200

    add some info

---
+++ cache/template/minbootstrap-y.php
@@ -0,0 +1,6 @@
+{
+    "tmpl_check": "d151eb66534e33637b48663b63407c4b",
+    "tmpl_data": "{\"status\":\"error\",\"message\":\"Sorry dude, this application is unknown.\"}",
+    "warp_check": "2c1ad966bd0dd1fad04b6aaa73f96bc2",
+    "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.18<\\\/a>\",\"version\":\"7.3.18\"}"
+}
